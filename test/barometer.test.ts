bbf877fedaa1b0cdbcb167f63c2a12dec6be4c75
Jeffrey Faucette <jfaucette1024@gmail.com>
2023-04-21 16:43:33 -0400

    Initial commit

---
+++ test/barometer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Barometer from '../lib/barometer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/barometer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Barometer.BarometerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
